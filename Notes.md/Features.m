1e434ca0b26c3286cf969de4d67ae643288e1ca7
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-08-11 00:37:09 +0530

    Update and rename Features to Features.m

---
+++ Notes.md/Features.m
@@ -0,0 +1,6 @@
+
+- Features Of a Language are nothing but services/facelities provided by language developers in the language , which are used by language programmers for developing real-time application.
+
+1. simple and easy to learn = Python language provides rich set of Modules(Collection Of Library) or API.
+2. Garbage Collector = Garbage collection
+Garbage collection is a memory management technique used in programming languages to automatically reclaim memory that is no longer accessible or in use by the application. It helps prevent memory leaks, optimize memory usage, and ensure efficient memory allocation for the program.
