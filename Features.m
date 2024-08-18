ad7af6e28cd577adfe2657a42594b22f8acde50b
manojv022 <167419795+manojv022@users.noreply.github.com>
2024-08-18 23:52:55 +0530

    Update Features.m

--- Features.m
+++ Features.m
@@ -6,4 +6,12 @@
 Garbage collection is a memory management technique used in programming languages to automatically reclaim memory that is no longer accessible or in use by the application. It helps prevent memory leaks, optimize memory usage, and ensure efficient memory allocation for the program.
 
 3. Freeware And OpenSource
-4. Dynamically typed programing
+4. Dynamically typed programing language 
+5. Platform Independent language
+6. Interperted programing language
+7. High level programing language 
+8. procedured and object oriented 
+9. Robust
+10. Extensible programing language
+11.Embedded programing language lanhuage
+12. support 3rd party APIs
